a7df7606fe0461abf596e91beed8f0ef0c414e1e
Paul  H <paul@paulsprojekte.de>
2020-11-07 21:11:01 +0100

    add count to api

--- api.py
+++ api.py
@@ -6,12 +6,15 @@ api = Blueprint('/api', __name__, url_prefix='/api')
 
 @api.route('/')
 def docs():
-    d = "GET /wrongquotes - get all wronguotes <br>"
-    d += "GET /wrongquotes/id - get wrongquote from id <br><br>"
-    d += "GET /quotes - get all quotes <br>"
-    d += "GET /quotes/id - get quote from id <br><br>"
-    d += "GET /authors - get all authors <br>"
-    d += "GET /authors/id - get author from id"
+    d = "GET /wrongquotes - get all wronguotes <br>" \
+        "GET /wrongquotes/<id> - get wrongquote from id <br>" \
+        "GET /wrongquotes/count - get wrongquote count <br><br>" \
+        "GET /quotes - get all quotes <br>" \
+        "GET /quotes/id - get quote from id <br>" \
+        "GET /quotes/count - get quote count <br><br>" \
+        "GET /authors - get all authors <br>" \
+        "GET /authors/id - get author from id <br>" \
+        "GET /authors/count - get author count"
     return d
 
 
@@ -28,6 +31,11 @@ def wrongquote(pk):
     return jsonify(WrongQuote.get_by_id(pk).get_dict())
 
 
+@api.route('/wrongquotes/count')
+def wrongquote_counts():
+    return jsonify(len(WrongQuote.select()))
+
+
 @api.route('/quotes')
 def quotes():
     quotes = []
@@ -41,6 +49,11 @@ def quote(pk):
     return jsonify(Quote.get_by_id(pk).get_dict())
 
 
+@api.route('/quotes/count')
+def quote_count():
+    return jsonify(len(Quote.select()))
+
+
 @api.route('/authors')
 def authors():
     quotes = []
@@ -52,3 +65,8 @@ def authors():
 @api.route('/authors/<int:pk>')
 def author(pk):
     return jsonify(Author.get_by_id(pk).get_dict())
+
+
+@api.route("/authors/count")
+def author_count():
+    return jsonify(len(Author.select()))
